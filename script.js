56f8659d64d670e32ec7d337914cc402c065c643
Ereklebazanovi <erekle.bazanovi.1@iliauni.edu.ge>
2024-11-12 13:09:44 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number!';
+
+// document.querySelector('.number').textContent = 33;
+
+// document.querySelector('.score').textContent = 22;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+/////////////////////////////////////////////////
+// გავხადოთ ინფუთი ჩექზე დაჭერისას ფუნქციონირებადი
+//EVENT LISTENER - ით
+//eventlistenerShi (პირველი არის ტაიფი ანუ რისი დამატება გვინდა როგორი ტაიფის) (მეორე არი რისი გაკეთება უნდა დავავალოთ მას,ფუნქციით რათქმაუნდა, ამ ფუნქციას ქვია event handler ი)
+
+document.querySelector('.check').addEventListener('click');
